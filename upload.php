cb238bce954e57167f4009d612f1906c3d36661b
Konstantin Mazurov <streeboge@yandex.ru>
2022-11-14 22:20:57 +0300

    delete index

--- upload.php
+++ upload.php
@@ -1,8 +1,6 @@
 <?php
 
 include_once __DIR__."/index.php";
-echo "<hr>";
-echo "<h2>Return:</h2>";
 
 if (!file_exists(__DIR__.'/vendor/autoload.php')) {
    throw new Exception("No vendor found, please run composer install --no-dev");
@@ -13,37 +11,25 @@ if (!file_exists(__DIR__.'/config.inc.php')) {
 }
 require_once __DIR__.'/config.inc.php';
 
-$base_uri = trim(GLPI_URL, '/');
+$base_uri = trim(GLPI_URL, '/').'/apirest.php';
 
 // define api client
-$api_client = new GuzzleHttp\Client(['base_uri' => "$base_uri/apirest.php/"]);
+$api_client = new \GuzzleHttp\Client();
 
-// connect to api
-$response = $api_client->get("initSession/", ['auth' => [API_USER, API_PASSWORD]]);
-if ($response->getStatusCode() != 200
-    || !$session_token = json_decode( (string) $response->getBody(), true)['session_token']) {
-   throw new Exception("Cannot connect to api, check your config.inc.php file");
-}
+$session_token = $_POST['session-token'];
+$filename  = $_POST['name'];
+$filepath  = './files/'.$_POST['name'];
 
-// check input
-if (!isset($_POST['document_name'])) {
-   die("No document name provided");
-}
-if (count($_FILES) == 0) {
-   die("No file provided");
-}
 // we may need more file control here, but for the example purpose, i skip
-
+file_put_contents($filepath, base64_decode($_POST['file']));
 // construct file keys
-$docname   = $_POST['document_name'];
-$inputname = array_keys($_FILES)[0];
-$filename  = $_FILES[$inputname]['name'][0];
-$filepath  = $_FILES[$inputname]['tmp_name'][0];
+$inputname = 'filename';
 
 // let's proceed a document addition
-$response = $api_client->post('Document/', [
+$response = $api_client->request('POST', $base_uri.'/Document/', [
    'headers' => [
-      'Session-Token' => $session_token
+      'Session-Token' => $session_token,
+       'App-Token' => 'k9geI9nm2O7jtgnwYyEnfK7jjYFWA152Q7Mtvxye',
    ],
    'multipart' => [
       // the document part
@@ -51,7 +37,7 @@ $response = $api_client->post('Document/', [
          'name'     => 'uploadManifest',
          'contents' => json_encode([
             'input' => [
-               'name'       => $docname,
+               'name'       => $filename,
                '_filename'  => [$filename],
             ]
          ])
@@ -69,10 +55,8 @@ $document_return = json_decode( (string) $response->getBody(), true);
 if ($response->getStatusCode() != 201) {
    throw new Exception("Error when sending file/document to api");
 }
+unlink($filepath);
 
-Echo "Document created:
-      <a target='_blank'
-         href='$base_uri/front/document.form.php?id=".$document_return['id']."'>
-         $docname
-      </a>";
-var_dump(json_decode( (string) $response->getBody()));
+header("Content-Type: application/json");
+echo (string) $response->getBody();
+
